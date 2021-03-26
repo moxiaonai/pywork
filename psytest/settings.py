dd823aa6ae455ca51da8a28c8bb704b34750079f
Moxiaonai <39784480@qq.com>
2021-03-26 23:19:44 +0800

    Update settings.py

--- psytest/settings.py
+++ psytest/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'psy_test',
         'USER': 'root',
-        'PASSWORD': 'wwj131410',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
